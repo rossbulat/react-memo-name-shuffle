f3e08341b38f4c33de2df97a7a7823fe7354b97c
Ross Bulat <ross@jkrbinvestments.com>
2019-10-22 21:06:02 +0100

    index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import AppFunctional from './AppFunctional';
+import App from './AppFunctional';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AppFunctional />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
